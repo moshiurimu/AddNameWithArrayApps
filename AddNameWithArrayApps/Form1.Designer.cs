f1fde3a7df21e637d4576ca5052a4737bd7a0343
moshiurimu <moshiurimu@gmail.com>
2014-09-29 17:01:39 +0600

    Using Array

---
+++ AddNameWithArrayApps/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace AddNameWithArrayApps
+{
+    partial class AddNameUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.resetButton);
+            this.groupBox1.Controls.Add(this.countLabel);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.addButton);
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(391, 86);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Name:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.nameListBox);
+            this.groupBox2.Controls.Add(this.showAllButton);
+            this.groupBox2.Location = new System.Drawing.Point(12, 104);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(391, 349);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Details";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name :";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(46, 27);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(218, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(270, 25);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(56, 24);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Add Counts :";
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countLabel.Location = new System.Drawing.Point(126, 60);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(0, 13);
+            this.countLabel.TabIndex = 4;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(332, 25);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(51, 24);
+            this.resetButton.TabIndex = 5;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(327, 11);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(56, 24);
+            this.showAllButton.TabIndex = 6;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.Location = new System.Drawing.Point(0, 41);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(391, 303);
+            this.nameListBox.TabIndex = 7;
+            // 
+            // AddNameUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 465);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "AddNameUI";
+            this.Text = "Add Name Apps";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.Button showAllButton;
+    }
+}
+
---
+++ AddNameWithArrayApps/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddNameWithArrayApps
+{
+    public partial class AddNameUI : Form
+    {
+        public AddNameUI()
+        {
+            InitializeComponent();
+        }
+
+        string[] name = new string[10];
+
+
+        int temp;
+        private void addButton_Click(object sender, EventArgs e)
+        {
+
+            name[temp] = nameTextBox.Text;
+            countLabel.Text = (temp+1).ToString();
+
+            temp++;
+            nameTextBox.Text = "";
+
+            if (countLabel.Text== "10")
+            {
+                addButton.Enabled = false;
+
+                nameListBox.Items.Clear();
+                for (int count = 0; count < name.Length; count++)
+                {
+                    nameListBox.Items.Add((count + 1) + ". " + name[count]);
+                }
+
+            }
+            
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            nameListBox.Items.Clear();
+
+            for (int count = 0; count < Convert.ToInt16(countLabel.Text); count++)
+            {
+                nameListBox.Items.Add((count+1)+ ". "+ name[count]);
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            addButton.Enabled = true;
+            nameListBox.Items.Clear();
+            countLabel.Text = "";
+
+            temp = 0;
+        }
+    }
+}
